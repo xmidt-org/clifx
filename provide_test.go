248aefe5adee0bd0535313704410b915d4a2adc9
johnabass <johnbass1017@yahoo.com>
2025-03-03 05:53:40 -0800

    unit tests to verify Provide's behavior

--- provide_test.go
+++ provide_test.go
@@ -6,8 +6,16 @@ package clifx
 import (
 	"testing"
 
+	"github.com/alecthomas/kong"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
 )
+
+type TestCLI struct {
+	Debug bool   `short:"d" default:"false"`
+	Value string `short:"v" required:"true"`
+}
 
 type ProvideTestSuite struct {
 	suite.Suite
@@ -20,6 +28,54 @@ func (suite *ProvideTestSuite) testNewNotAStruct() {
 
 func (suite *ProvideTestSuite) TestNew() {
 	suite.Run("NotAStruct", suite.testNewNotAStruct)
+}
+
+func (suite *ProvideTestSuite) testProvideSimpleUsage() {
+	var cli TestCLI
+	var kctx *kong.Context
+
+	fxtest.New(
+		suite.T(),
+		fx.NopLogger,
+		Provide[TestCLI](
+			AsArguments("-v", "foo"),
+			SuppressExit(),
+		),
+		fx.Populate(&cli, &kctx),
+	)
+
+	suite.Equal(
+		TestCLI{
+			Value: "foo",
+		},
+		cli,
+	)
+
+	suite.NotNil(kctx)
+}
+
+func (suite *ProvideTestSuite) testProvideSuppressExit() {
+	var cli TestCLI
+	var kctx *kong.Context
+
+	app := fx.New(
+		fx.NopLogger,
+		Provide[TestCLI](
+			AsArguments("--invalid", "not a valid value"),
+			SuppressExit(),
+		),
+		fx.Populate(&cli, &kctx),
+	)
+
+	suite.Error(app.Err())
+
+	var pe *kong.ParseError
+	suite.ErrorAs(app.Err(), &pe)
+}
+
+func (suite *ProvideTestSuite) TestProvide() {
+	suite.Run("SimpleUsage", suite.testProvideSimpleUsage)
+	suite.Run("SuppressExit", suite.testProvideSuppressExit)
 }
 
 func TestProvide(t *testing.T) {
